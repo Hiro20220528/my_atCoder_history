605e07f14fcb30bd52d83260dc05f7bd8a29b6c6
Okami Hiro <yy20soccer@gmail.com>
2023-10-15 15:52:02 +0900

    dの全探索

--- abc/324/d.py
+++ abc/324/d.py
@@ -1,23 +1,40 @@
-import itertools
-
 N = int(input())
 S = input()
 
+max_num = 0
+for i in range(N):
+          max_num += 9 * 10 ** i
+
 moji = list(S)
 
-p_list = list(itertools.permutations(moji))
-
-h = {}
-# あらかじめ計算する
-for i in range(10**6):
-          h[i**2] = True
-          
+history = {}
 ans = 0
-for each in p_list:
-          num = int(''.join(each))
-          # print(num)
-          if h.get(num, False):
-                    h[num] = False
+# あらかじめ計算する
+for i in range(10**7):
+          h = {}
+          for num in moji:
+                    h[num] = h.get(num, 0) + 1
+          
+
+          nijou = i**2
+          if nijou > max_num:
+                    break
+          
+          flag = True
+          
+          nijou_str = str(nijou)
+          nijou_str = list(nijou_str)
+          nijou_str = ['0'] * (N - len(nijou_str)) + nijou_str
+          
+          for chr in nijou_str:
+                    if h.get(chr, 0) > 0:
+                              h[chr] -= 1
+                    else:
+                              flag = False
+                              break
+                    
+          if flag and history.get(nijou, True):
+                    history[nijou] = False
                     ans += 1
                     
 print(ans)
